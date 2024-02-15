272d117b901d6809e0b07a6af060539ea8c4fd2e
Jason Gan <ganjason@usc.edu>
2024-02-15 12:16:32 -0800

    llrecSolved

--- llrec.cpp
+++ llrec.cpp
@@ -5,3 +5,51 @@
 //*********************************************
 
 
+void llpivot(Node *&head, Node *&smaller, Node *&larger, int pivot)
+{
+    Node* smallerTail = nullptr; 
+    Node* largerTail = nullptr;
+    smaller = larger = nullptr;
+    llpivotHelper(head, smaller, larger, smallerTail, largerTail, pivot);
+    head = nullptr;
+
+}
+
+void llpivotHelper(Node *&current, Node *&smaller, Node *&larger, Node *&smallerTail, Node *&largerTail, int pivot)
+{
+    if (current = nullptr)
+    {
+        if (smallerTail != nullptr) smallerTail->next = nullptr;
+        if (largerTail != nullptr) largerTail->next = nullptr;
+        return;
+    }
+
+    Node* next = current->next;
+    if (current->val <= pivot)
+    {
+        if (smaller == nullptr)
+        {
+            smaller = smallerTail = current;
+        }
+        else
+        {
+            smallerTail->next = current;
+            smallerTail = current;
+        }
+    }
+    else
+    {
+        if (larger == nullptr)
+        {
+            larger = largerTail = current;
+        }
+        else
+        {
+            largerTail->next = current;
+            largerTail = current;
+        }
+    }
+    
+    llpivotHelper(next, smaller, larger, smallerTail, largerTail, pivot);
+
+}
